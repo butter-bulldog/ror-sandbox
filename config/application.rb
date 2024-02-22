3980b71b8a7fb18e9153efd885302ceb0ffef525
Fumiaki Saito <wiseman.fumiaki.saito@gmail.com>
2024-02-23 00:00:09 +0900

    update erb -> slim

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RorSandbox
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators.template_engine = :slim #slimに変更
   end
 end
